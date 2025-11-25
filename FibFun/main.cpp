e2f8273e4c2b0eb980290333d09803be9bb1c749
lamalas600 <henryhawes1994@gmail.com>
2025-11-25 19:27:34 +0000

    Incorporated Memoization

--- FibFun/main.cpp
+++ FibFun/main.cpp
@@ -7,8 +7,24 @@
 
 #include <iostream>
 
+long long fib(int x, std::vector<long long>& memo){
+    if (x <= 1){
+        return x;
+    }
+    if(memo[x] != -1){
+        return memo[x];
+    }
+    memo[x] = fib(x-1, memo) + fib(x-2, memo);
+    return memo[x];
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int x = 8;
+    std::vector<long long> memo(x+1, -1);
+    std::cout << fib(x, memo) << std::endl;
+    std::cout << "fib(x, memo)" << std::endl;
     return EXIT_SUCCESS;
 }
+
+
